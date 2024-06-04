9c94598c11220b3167d714a16775b593fed1408d
Jorique <guselnikovgeorgiy@gmail.com>
2024-06-05 00:54:32 +0300

    added if main run

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
+import uvicorn
 from fastapi import FastAPI
 
 from app.storage.router import router as storage_router
 
 
 app = FastAPI(
-    docs_url="/api/docs",
     root_path="/api",
     title="Store service",
     description="This service stores and processes data",
@@ -12,3 +12,6 @@ app = FastAPI(
 )
 
 app.include_router(storage_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
