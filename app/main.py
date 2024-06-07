e2169455c325cd32848eaeaaca32121a504a84b3
Jorique <guselnikovgeorgiy@gmail.com>
2024-06-07 15:16:01 +0300

    changed main func

--- app/main.py
+++ app/main.py
@@ -1,17 +1,16 @@
-import uvicorn
 from fastapi import FastAPI
 
 from app.storage.router import router as storage_router
 
 
-app = FastAPI(
-    root_path="/api",
-    title="Store service",
-    description="This service stores and processes data",
-    debug=True,
-)
+def create_app() -> FastAPI:
+    app = FastAPI(
+        root_path="/api",
+        title="Store service",
+        description="This service stores and processes data",
+        debug=True,
+    )
 
-app.include_router(storage_router)
+    app.include_router(storage_router)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    return app
