f4829ea7eb8bcd6cd2416872cfe92e8854c6690b
Jorique <guselnikovgeorgiy@gmail.com>
2024-06-08 00:44:26 +0300

    added post and get endpoints

--- app/storage/router.py
+++ app/storage/router.py
@@ -1,14 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Depends
 
+from app.storage.database import db_client
+from app.storage.models import UserDataInsert, UserDataGet
+from app.storage.schemas import UserDataFilter
 
 router = APIRouter()
 
 
-@router.get("/traces/all")
-async def get_all_traces():
-    return {"message": "Hello World"}
+@router.get(
+    "/user_data",
+    status_code=status.HTTP_200_OK,
+    description="Эндпоинт для получения записей из базы данных по параметрам фильтрации",
+)
+async def get_filtered_user_data(
+        filter_params: UserDataFilter = Depends(),
+) -> list[UserDataGet]:
+    filter_query = filter_params.dict(exclude_none=True)
+    return await db_client.find_user_data(filter_query)
 
 
-@router.get("/traces/{ip}")
-async def get_trace(trace_ip):
-    return {"message": f"trace for {trace_ip}"}
+@router.post(
+    "/user_data",
+    status_code=status.HTTP_201_CREATED,
+    description="Эндпоинт для добавления записей в базу данных",
+)
+async def insert_user_data(user_data: UserDataInsert) -> dict:
+    return await db_client.insert_user_data(user_data)
+
