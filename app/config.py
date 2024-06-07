9980a148c2562af61a8a0ffd844adc0a50609f0c
Jorique <guselnikovgeorgiy@gmail.com>
2024-06-08 00:42:41 +0300

    added env vars

--- app/config.py
+++ app/config.py
@@ -5,9 +5,13 @@ class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
+    APP_HOST: str
+    APP_PORT: int
+    MONGO_DB_ADMIN_USERNAME: str
+    MONGO_DB_ADMIN_PASSWORD: str
 
     @property
-    def DATABASE_URL(self):
+    def DATABASE_URL(self) -> str:
         return f"mongodb://{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     model_config = SettingsConfigDict(env_file=".env")
