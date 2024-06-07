6fb083efe352e5e65196fb85c38793c301b74f69
Jorique <guselnikovgeorgiy@gmail.com>
2024-06-08 00:42:09 +0300

    changed main func

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from app.storage.router import router as storage_router
 def create_app() -> FastAPI:
     app = FastAPI(
         root_path="/api",
-        title="Store service",
+        title="Storage Service",
         description="This service stores and processes data",
         debug=True,
     )
