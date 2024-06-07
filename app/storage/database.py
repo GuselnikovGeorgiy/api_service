ca8d22a4fc9e177d150c604bec009987a4a222d0
Jorique <guselnikovgeorgiy@gmail.com>
2024-06-08 00:46:14 +0300

    added create and read w\ filtering actions

--- app/storage/database.py
+++ app/storage/database.py
@@ -1,10 +1,43 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from dataclasses import dataclass
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 
-from app.config import settings
+from app.storage.models import UserDataGet, UserDataInsert
+from app.storage.utils import split_query, calculate_connection_duration
 
 
-client = AsyncIOMotorClient(settings.DATABASE_URL)
+@dataclass
+class MongoDBClient:
+    mongo_db_client: AsyncIOMotorClient
+    mongo_db_name: str
+    mongo_collection_name: str
+
+    def _get_collection(self) -> AsyncIOMotorCollection:
+        return self.mongo_db_client[self.mongo_db_name][self.mongo_collection_name]
+
+    async def find_user_data(self, filter_params: dict) -> list[UserDataGet]:
+        limit = filter_params.get("limit")
+
+        filter_query = split_query(filter_params)
+
+        cursor = self._get_collection().find(filter_query)
+
+        if limit is not None:
+            cursor = cursor.limit(limit)
+        result = await cursor.to_list(length=None)
+        for document in result:
+            if "_id" in document:
+                document["_id"] = str(document["_id"])
+        return result
+
+    async def insert_user_data(self, user_data: UserDataInsert) -> dict:
+        document = user_data.dict()
+        document = calculate_connection_duration(document)
+        result = await self._get_collection().insert_one(document)
+        return {"id": str(result.inserted_id)}
 
 
-async def get_database() -> AsyncIOMotorClient:
-    return client
+db_client = MongoDBClient(
+    mongo_db_client=AsyncIOMotorClient("mongodb://mongodb:27017", serverSelectionTimeoutMS=3000),
+    mongo_db_name="user_data_storage",
+    mongo_collection_name="user_data",
+)
