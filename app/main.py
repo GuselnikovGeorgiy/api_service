4d7878cc2f8956017fa9edb13f6e762a8754497e
Jorique <guselnikovgeorgiy@gmail.com>
2024-06-19 18:18:30 +0300

    changed static docs route

--- app/main.py
+++ app/main.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
 
 from app.storage.router import router as storage_router
+
+
+def register_static_docs_routes(app: FastAPI):
+    @app.get("/docs", include_in_schema=False)
+    async def custom_swagger_ui_html():
+        return get_swagger_ui_html(
+            openapi_url=app.openapi_url,
+            title=app.title + " - Swagger UI",
+            oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+            swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+            swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+        )
+
+    @app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+    async def swagger_ui_redirect():
+        return get_swagger_ui_oauth2_redirect_html()
+
+    @app.get("/redoc", include_in_schema=False)
+    async def redoc_html():
+        return get_redoc_html(
+            openapi_url=app.openapi_url,
+            title=app.title + " - ReDoc",
+            redoc_js_url="https://unpkg.com/redoc@next/bundles/redoc.standalone.js",
+        )
 
 
 def create_app() -> FastAPI:
@@ -9,6 +38,8 @@ def create_app() -> FastAPI:
         root_path="/api",
         title="Storage Service",
         description="This service stores and processes data",
+        docs_url=None,
+        redoc_url=None,
         debug=True,
     )
 
@@ -18,8 +49,10 @@ def create_app() -> FastAPI:
         CORSMiddleware,
         allow_origins=["*"],
         allow_credentials=True,
-        allow_methods=["*"],
+        allow_methods=["GET", "POST"],
         allow_headers=["*"],
     )
+
+    register_static_docs_routes(app)
 
     return app
